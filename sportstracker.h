673be3c1546eff6aa3184a07a991c04bd9c06582
KKKochnev <Ekochnev23@yandex.ru>
2025-06-28 01:33:08 +0300

    Add files via upload

--- sportstracker.h
+++ sportstracker.h
@@ -9,7 +9,6 @@
 #include <QPushButton>
 #include <QSqlDatabase>
 #include <QLabel>
-#include <QTabWidget>
 
 class SportsTracker : public QMainWindow
 {
@@ -22,11 +21,6 @@ public:
 private slots:
     void showTournamentPage(int tournamentId, const QString &tournamentName);
     void onTournamentClicked(QTreeWidgetItem *item, int column);
-    void showRoundSelectionPopup();
-    void onRoundSelected(QAbstractButton *button);
-    void loadMatchesForCurrentRound();
-    void loadMatchesAndStandings();
-    void loadStandings();
 
 private:
     void setupUI();
@@ -36,39 +30,19 @@ private:
     void loadMatchesAndStandings(int tournamentId);
     void showMatchStats(QListWidgetItem *item);
     void showMatchesList();
-    void loadMatchStats(int matchId, const QString& team1, const QString& team2);
-    void loadLineups(int matchId, const QString& team1, const QString& team2);
-    void loadScorers(int matchId, const QString& team1, const QString& team2);
-    void loadRecentMatches(const QString& team, const QDate& beforeDate, QTableWidget* table);
-    void loadHeadToHeadMatches(const QString& team1, const QString& team2, const QDate& beforeDate, QTableWidget* table);
 
     QStackedWidget *stackedWidget;
     QTreeWidget *sportsTree;
     QListWidget *matchesList;
     QTableWidget *standingsTable;
     QTableWidget *statsTable;
-    QTableWidget *lineupsTable;
-    QTableWidget *scorersTable;
-    QTableWidget *team1RecentMatches;
-    QTableWidget *team2RecentMatches;
-    QTableWidget *headToHeadMatches;
     QLabel *matchTitle;
     QPushButton *backButton1;
     QPushButton *backButton2;
-    QStackedWidget *leftPanelStack;
-    QTabWidget *statsTabs;
-    QWidget *matchOverviewTab;
-    QWidget *historyTab;
+    QWidget *matchesPanel;  // Добавлено для управления отображением
+    QStackedWidget *leftPanelStack;  // Добавлено для переключения между списком матчей и статистикой
     int currentTournamentId;
     QString currentTournamentName;
-    int currentRound;
-    int currentRoundPage;
-    int roundsPerPage;
-    QList<int> allRounds;
-    QWidget *roundsPopup;
-    QPushButton *roundButton;
-    QButtonGroup *roundsGroup;
-    int currentMatchId;
     QSqlDatabase db;
 };
 
